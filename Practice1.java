1d197446b9d354fd464b6b28eca38b803afc685f
Prachi Ikhar <prachiikhar42@gmail.com>
2025-02-02 22:03:04 +0530

    Added a file

--- Practice1.java
+++ Practice1.java
@@ -1,8 +1,11 @@
+package LeetCodeArray;
 
 import java.util.*;
 
-public class Practice {
+public class Practice1 {
 	public static void main(String args[]) {
-		System.out.println("Hi");
+		int n = 10;
+		System.out.println("n % 2 = " + n % 2);
+		System.out.println("2 % n = " + 2 % n);
 	}
 }
