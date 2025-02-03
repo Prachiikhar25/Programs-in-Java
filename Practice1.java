8c0ac92d5b449c6f02c9cadb1d09c64d5da99b49
Prachi Ikhar <prachiikhar42@gmail.com>
2025-02-03 21:44:04 +0530

    Added a file

--- Practice1.java
+++ Practice1.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Practice1 {
 	public static void main(String args[]) {
 		int n = 10;
-		System.out.println("n % 2 = " + n % 2);
-		System.out.println("2 % n = " + 2 % n);
+		System.out.println("n % 2 = " + n % 2 + " and n = " + n);
+		System.out.println("8 % 7 = " + 8 % 7);
 	}
 }
