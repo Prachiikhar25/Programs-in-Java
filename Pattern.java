5b1dd8284c6f3c4ea05aa70dbc613ffedb1e3774
Prachi Ikhar <prachiikhar42@gmail.com>
2025-02-07 17:57:46 +0530

    modification in case in Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -3,7 +3,7 @@ package LeetCodeArray;
 public class Pattern {
 	public static void main(String args[]) {
 		// print a pattern
-		char ch = 'a';
+		char ch = 'A';
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j <= i; j++) {
 				System.out.print(ch + " ");
