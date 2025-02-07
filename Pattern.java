dc420215603abcd65454ac47bf8445fe7fc92e81
Prachi Ikhar <prachiikhar42@gmail.com>
2025-02-07 17:57:14 +0530

    Print a pattern a, b, c..

---
+++ Pattern.java
@@ -0,0 +1,15 @@
+package LeetCodeArray;
+
+public class Pattern {
+	public static void main(String args[]) {
+		// print a pattern
+		char ch = 'a';
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print(ch + " ");
+				ch++;
+			}
+			System.out.println();
+		}
+	}
+}
